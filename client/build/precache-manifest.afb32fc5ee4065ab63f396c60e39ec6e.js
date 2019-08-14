69854a140016fd60b8a58878c20c22356dd5378b
Elvy Kiung <kiungh22@gmail.com>
2019-08-14 00:09:35 -0700

    build

---
+++ client/build/precache-manifest.afb32fc5ee4065ab63f396c60e39ec6e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "10f9ce0e44a54c0c73de",
+    "url": "/static/js/main.1752e6e8.chunk.js"
+  },
+  {
+    "revision": "f58ed82d8c867b09c4bf",
+    "url": "/static/js/2.7700d018.chunk.js"
+  },
+  {
+    "revision": "f754df8d279a6354ed6b4245ebd7e15e",
+    "url": "/index.html"
+  }
+];
